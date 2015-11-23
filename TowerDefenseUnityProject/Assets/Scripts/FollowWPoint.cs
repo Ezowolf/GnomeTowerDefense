f1f41b3296c6d5bc5e253799a1eb579953264325
Ezowolf <wiggler@live.nl>
2015-11-23 17:47:59 +0100

    Art implementation

---
+++ TowerDefenseUnityProject/Assets/Scripts/FollowWPoint.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowWPoint : MonoBehaviour {
+	public GameObject[] Waypoints;
+	private int currentWaypoint = 0;
+	public float speed = 1.0f;
+	private Vector3 myPosition;
+	private Rigidbody2D m_Rigidbody2D;
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Flip();
+		myPosition = this.transform.position;
+		if(myPosition==Waypoints[currentWaypoint].transform.position)
+		{
+			if (currentWaypoint == Waypoints.Length-1)
+			{
+				Destroy(this.gameObject);
+			}
+			else
+			{
+				currentWaypoint++;
+			}
+		}
+		transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), Waypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
+
+	}
+	private void Flip()
+	{
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
+
